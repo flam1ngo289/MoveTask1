5a60d3fef5ca12a5351a7977d6945c00f46fbe1d
flam1ngo <kris.s.03@mail.ru>
2024-06-18 19:25:19 +0300

    Initial commit

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <utility> 
+#include <Windows.h>
+
+
+template <typename T>
+void move_vectors(std::vector<T>& source, std::vector<T>& destination) {
+    destination = std::move(source);
+}
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    std::vector<std::string> one = { "test_string1", "test_string2" };
+    std::vector<std::string> two;
+
+    std::cout << "Вектор one до перемещения: ";
+    for (const auto& s : one) {
+        std::cout << s << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Вектор two до перемещения: ";
+    for (const auto& s : two) {
+        std::cout << s << " ";
+    }
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+
+    move_vectors(one, two);
+
+    
+    std::cout << "Вектор one после перемещения: ";
+    for (const auto& s : one) {
+        std::cout << s << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Вектор two после перемещения: ";
+    for (const auto& s : two) {
+        std::cout << s << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
